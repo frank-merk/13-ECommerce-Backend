d1e080f46feff4e3add0ed11a480bd24379b6982
Frank Merchlewitz <jlimhb@gmail.com>
2021-03-16 22:14:22 -0500

    Category Model init

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
   {
     sequelize,
@@ -18,3 +27,28 @@ Category.init(
 );
 
 module.exports = Category;
+
+// // create fields/columns for Location model
+// Location.init(
+//   {
+//     id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       primaryKey: true,
+//       autoIncrement: true
+//     },
+//     location_name: {
+//       type: DataTypes.STRING,
+//       allowNull: false
+//     }
+//   },
+//   {
+//     sequelize,
+//     timestamps: false,
+//     freezeTableName: true,
+//     underscored: true,
+//     modelName: 'location'
+//   }
+// );
+
+// module.exports = Location;
