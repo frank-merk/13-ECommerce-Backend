f851f9f7f73d686038dd9a749c6299f7595fb935
Frank Merchlewitz <jlimhb@gmail.com>
2021-03-18 00:00:31 -0500

    all post routes work

--- models/Product.js
+++ models/Product.js
@@ -20,11 +20,9 @@ Product.init(
       allowNull: false,
     },
     price: {
-      type: DataTypes.DECIMAL(10.2),
+      type: DataTypes.DECIMAL,
       allowNull: false,
-      validate: {
-        isDecimal: true
-      }
+      
     },
     stock: {
       type: DataTypes.INTEGER,
