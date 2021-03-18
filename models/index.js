ee969724c4e746a066f7788cf5cfcc10fbd0a444
Frank Merchlewitz <jlimhb@gmail.com>
2021-03-17 23:17:32 -0500

    Debug commit, get all working in postman

--- models/index.js
+++ models/index.js
@@ -15,7 +15,7 @@ Category.hasMany(Product);
 Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
-    foreignKey: product_id
+    foreignKey: 'product_id'
   }
 });
 
@@ -23,7 +23,7 @@ Product.belongsToMany(Tag, {
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
-    foreignKey: tag_id
+    foreignKey: 'tag_id'
   }
 });
 
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -44,7 +44,7 @@ router.get('/:id', async (req, res) => {
 });
 
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   try{
     const tagData = await Tag.create(req.body);
     res.status(200).json(tagData);
