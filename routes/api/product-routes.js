d8aa4dbb0cbf2cac0a063f975fc6b0307e15aab6
Frank Merchlewitz <jlimhb@gmail.com>
2021-03-17 22:43:13 -0500

    Forgot to hit save

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -110,7 +110,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   try {
     const productData = await Product.destroy({
       where: {
